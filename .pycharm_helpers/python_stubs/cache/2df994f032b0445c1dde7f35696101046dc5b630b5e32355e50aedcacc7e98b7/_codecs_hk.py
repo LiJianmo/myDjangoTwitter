2ffc6892770012f62628e36ab7a02fc1424b9a3f
李简墨 <lijianmo@lijianmodeMacBook-Pro.local>
2021-10-31 22:10:23 -0400

    why .DS_Store doesn't be upload

---
+++ .pycharm_helpers/python_stubs/cache/2df994f032b0445c1dde7f35696101046dc5b630b5e32355e50aedcacc7e98b7/_codecs_hk.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+# module _codecs_hk
+# from /usr/lib/python3.6/lib-dynload/_codecs_hk.cpython-36m-x86_64-linux-gnu.so
+# by generator 1.147
+# no doc
+# no imports
+
+# functions
+
+def getcodec(*args, **kwargs): # real signature unknown
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7fc8d04a3a90>'
+
+__map_big5hkscs = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf3751b0>'
+
+__map_big5hkscs_bmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf375180>'
+
+__map_big5hkscs_nonbmp = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf3752a0>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='_codecs_hk', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7fc8d04a3a90>, origin='/usr/lib/python3.6/lib-dynload/_codecs_hk.cpython-36m-x86_64-linux-gnu.so')"
+
---
+++ .pycharm_helpers/python_stubs/cache/93f7fdfb2c507c40de71f3d756a698625d635a4c0980fd7bc793858d4d825c80/_codecs_cn.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+# module _codecs_cn
+# from /usr/lib/python3.6/lib-dynload/_codecs_cn.cpython-36m-x86_64-linux-gnu.so
+# by generator 1.147
+# no doc
+# no imports
+
+# functions
+
+def getcodec(*args, **kwargs): # real signature unknown
+    pass
+
+# no classes
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x7fc8d04a3a90>'
+
+__map_gb18030ext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf3752d0>'
+
+__map_gb2312 = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf3751b0>'
+
+__map_gbcommon = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf3752a0>'
+
+__map_gbkext = None # (!) real value is '<capsule object "multibytecodec.__map_*" at 0x7fc8cf375180>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='_codecs_cn', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x7fc8d04a3a90>, origin='/usr/lib/python3.6/lib-dynload/_codecs_cn.cpython-36m-x86_64-linux-gnu.so')"
+
