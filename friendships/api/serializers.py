2c9f8b43d0074ed99be7b80bfe8e3a3f49598da5
LiJianmo <jianmo.li@mail.mcgill.ca>
2022-03-09 18:30:21 -0500

    14-pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -3,6 +3,7 @@ from friendships.models import Friendship
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import User
+from friendships.services import FriendshipService
 
 class FollowerSerializer(serializers.ModelSerializer):
     #一般是UserSerializerForFriendship 但现在就是去model中 找Friendship中的from_user内容
@@ -10,17 +11,47 @@ class FollowerSerializer(serializers.ModelSerializer):
     #可以不用有默认
     created_at = serializers.DateTimeField()
 
+    has_followed = serializers.SerializerMethodField()
+
+
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, object):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        #from_user: 当前登录的user 即self.context['request'].user
+        #to_user: 关注xx的user xx是object from_user是xx的follower（粉丝）
+        #but every object nedds to be check by sql
+        return FriendshipService.has_followed(self.context['request'].user, object.from_user)
+
+
+
+
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
     created_at = serializers.DateTimeField()
 
+    has_followed = serializers.SerializerMethodField()
+
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, object):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        return FriendshipService.has_followed(self.context['request'].user, object.to_user)
+
+
+
+
+
+
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
     from_user_id = serializers.IntegerField()
--- friendships/services.py
+++ friendships/services.py
@@ -5,13 +5,23 @@ class FriendshipService(object):
 
     @classmethod
     def get_followers(cls, user):
-        #或者用prefetch_related 可以变两条 1.取friendship 2.取id查询
+        #或者用prefetch_related 可以变两条 1.取friendship 2.取id查
+        # 正确的写法一，自己手动 filter id，使用 IN Query 查询
         # friendships = Friendship.objects.filter(to_user=user)
         # follower_ids = [friendship.from_user_id for friendship in friendships]
-        # followers = User.objects.filter(id_in = follower_ids)
+        #.from_user_id 不会产生新的sql查询,本身存在table里面
+        # followers = User.objects.filter(id__in=follower_ids)
 
         friendships = Friendship.objects.filter(
             to_user=user,
         ).prefetch_related('from_user')
 
         return [friendship.from_user for friendship in friendships]
+
+    @classmethod
+    def has_followed(cls, from_user, to_user):
+        return Friendship.objects.filter(
+            from_user=from_user,
+            to_user=to_user,
+        ).exists()
+
